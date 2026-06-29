3c72c65a74bb1c221cdca2c6e5a8532624e289e1
acedash <mailtokhanasrar@gmail.com>
2026-06-30 00:04:24 +0530

    aded

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Admin User',
-            'email' => 'admin@astryxacademy.com',
+            'email' => 'admin@skillstryx.com',
+            'password' => bcrypt('password'),
             'role' => 'admin',
         ]);
 
